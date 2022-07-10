cc8328e68907b48e9884b2ee32f9d5c7fe5a5881
dimon08888 <dimon08888@mail.ru>
2022-07-10 15:54:41 +0300

    Добавлена таблица с расширнеием колонок

---
+++ src/TableContent.jsx
@@ -0,0 +1,61 @@
+import React from 'react'
+
+const TableContent = () => {
+  return (
+    <tbody>
+      <tr>
+        <td>
+          <span>Pizza</span>
+        </td>
+        <td>
+          <span>3213456785</span>
+        </td>
+        <td>
+          <span>1231232131.43</span>
+        </td>
+        <td>
+          <span>Lorem</span>
+        </td>
+        <td>
+          <span>john doe</span>
+        </td>
+      </tr>
+      <tr>
+        <td>
+          <span>Pizza</span>
+        </td>
+        <td>
+          <span>3213456785</span>
+        </td>
+        <td>
+          <span>1231232131.43</span>
+        </td>
+        <td>
+          <span>Lorem</span>
+        </td>
+        <td>
+          <span>john doe</span>
+        </td>
+      </tr>
+      <tr>
+        <td>
+          <span>Pizza</span>
+        </td>
+        <td>
+          <span>3213456785</span>
+        </td>
+        <td>
+          <span>1231232131.43</span>
+        </td>
+        <td>
+          <span>Lorem</span>
+        </td>
+        <td>
+          <span>john doe</span>
+        </td>
+      </tr>
+    </tbody>
+  )
+}
+
+export default TableContent
